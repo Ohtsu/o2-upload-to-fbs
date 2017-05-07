839e13aef255ace37896683037693e644d19bac1
Shuichi Ohtsu <ohtsu@digipub-net.com>
2017-05-07 18:40:08 +0900

    Add Many Parameters

--- src/o2-upload-to-fbs.component.ts
+++ src/o2-upload-to-fbs.component.ts
@@ -5,7 +5,9 @@ import { FirebaseApp } from 'angularfire2';
   selector: 'o2-upload-to-fbs',
   template: `
   <input type="file" id="btnUpload" style="display:none;"  (change)="onBtnUploadChange($event)" />
+  <input [(ngModel)]="fileUploadFileName" placeholder={{inpPlaceholderText}} Size="30">
   <button type="button" (click) = "onBtnBrowseClick($event)">{{btnSelectText}}</button>
+  <button type="button" (click) = "onBtnExecuteClick($event)">{{btnExecuteText}}</button>
   `
 //   ,
 //   styleUrls: ['./o2-upload-to-fbs.component.css']
@@ -13,19 +15,28 @@ import { FirebaseApp } from 'angularfire2';
 export class O2UploadToFbsComponent implements OnInit {
     @Input() fbsBasePath: string;
     @Input() btnSelectText: string;
-     targetRef:any;
-     storageRef:any;
+    @Input() btnExecuteText: string;
+    @Input() inpPlaceholderText: string;
+    @Input() flgShowMessage: boolean;
+     fileUploadFileName: any;
+     fileUploadFile: any;
+     targetRef: any;
+     storageRef: any;
      constructor(@Inject(FirebaseApp) firebaseApp:any){
         this.storageRef = firebaseApp.storage().ref();
+        this.flgShowMessage = true;
      }
 
      ngOnInit() {
          if (this.btnSelectText == ''){
            this.btnSelectText = "Select File";
          }
+         if (this.btnExecuteText == ''){
+           this.btnSelectText = "Upload";
+         }
      }
 
-     
+
      onBtnBrowseClick(event:any){
          let uploadButton = document.getElementById("btnUpload");
          uploadButton.click();
@@ -35,10 +46,16 @@ export class O2UploadToFbsComponent implements OnInit {
          let targetFile = event.srcElement.files[0];
          let uploader = document.getElementById("btnUpload");
          let fbsPath = this.fbsBasePath + targetFile.name;
-         this.uploadFile(fbsPath,targetFile);
+         this.fileUploadFileName = targetFile.name;
+         this.fileUploadFile = targetFile;
      }
 
-     uploadFile(fbsPath:string,targetFile:string) {
+     onBtnExecuteClick(event:any){
+        let fbsPath = this.fbsBasePath + this.fileUploadFileName;
+        this.uploadFile(fbsPath,this.fileUploadFile);
+     }
+
+     uploadFile(fbsPath:string,targetFile:any) {
        let promise = new Promise((res,rej) => {
           this.targetRef=this.storageRef.child(fbsPath);
           let task=this.targetRef.put(targetFile);
@@ -48,11 +65,19 @@ export class O2UploadToFbsComponent implements OnInit {
             },
             (error:any) => {
               console.log(error.code);
+              if(this.flgShowMessage){
+                let msg = "Error "+error.code;
+                window.alert(msg);
+              }
               rej(error);
             },
             () => {
               let downloadUrl = task.snapshot.downloadURL;
               console.log(downloadUrl);
+              if(this.flgShowMessage){
+                let msg = "Uploaded ---> "+fbsPath;
+                window.alert(msg);
+              }
               res(downloadUrl);
             }
           );
